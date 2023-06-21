1d438d1604062ee9f1c4ca3e8d390d68349479a7
Sam Lu <pbcmarsh@gmail.com>
2023-06-21 17:22:42 -0500

    Update Number of Shipments Per Month.sql
    fix column naming for year_month

--- PostgreSQL/Number of Shipments Per Month.sql
+++ PostgreSQL/Number of Shipments Per Month.sql
@@ -1,11 +1,11 @@
 /*
 ID 2056
 Problem Name: Number of Shipments Per Month
-Link: https://platform.stratascratch.com/coding/2056-number-of-shipments-per-month?code_type=1
+Link: https://platform.stratascratch.com/coding/2056-number-of-shipments-per-month
 Tool(s) Used: PostgreSQL
 */
 SELECT
-TO_CHAR(shipment_date, 'yyyy-MM'),
+TO_CHAR(shipment_date, 'yyyy-MM') AS year_month,
 COUNT(EXTRACT(MONTH FROM shipment_date))
 FROM
 amazon_shipment
