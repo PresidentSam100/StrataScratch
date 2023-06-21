3f0eaaa78b4ad48aa8208bf5d32f0ac7db3d56d6
Sam Lu <pbcmarsh@gmail.com>
2023-06-21 18:29:16 -0500

    Create Find all posts which were reacted to with a heart.sql

---
+++ PostgreSQL/Find all posts which were reacted to with a heart.sql
@@ -0,0 +1,16 @@
+/*
+ID 10087
+Problem Name: Find all posts which were reacted to with a heart
+Link: https://platform.stratascratch.com/coding/10087-find-all-posts-which-were-reacted-to-with-a-heart?code_type=1
+Tool(s) Used: PostgreSQL
+*/
+SELECT DISTINCT
+fp.*
+FROM
+facebook_posts AS fp
+INNER JOIN
+facebook_reactions AS fr
+ON
+fp.post_id = fr.post_id
+WHERE
+reaction = 'heart'
