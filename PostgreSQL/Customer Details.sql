422ef72f01f4a4691e9e6b0d128b5a7bc5e4f02f
Sam Lu <pbcmarsh@gmail.com>
2023-06-21 16:54:39 -0500

    Update Customer Details.sql
    Replace old solution with new solution

--- PostgreSQL/Customer Details.sql
+++ PostgreSQL/Customer Details.sql
@@ -1,14 +1,20 @@
+/*
+ID 9891
+Problem Name: Customer Details
+Link: https://platform.stratascratch.com/coding/9891-customer-details?code_type=1
+Tool(s) Used: PostgreSQL
+*/
 SELECT 
-customers.first_name,
-customers.last_name, 
-customers.city,
-orders.order_details
+c.first_name,
+c.last_name, 
+c.city,
+o.order_details
 FROM
-customers
+customers AS c
 LEFT JOIN
-orders
+orders AS o
 ON
-customers.id = orders.cust_id
+c.id = o.cust_id
 ORDER BY 
-customers.first_name ASC,
-orders.order_details ASC;
+c.first_name ASC,
+o.order_details ASC;
