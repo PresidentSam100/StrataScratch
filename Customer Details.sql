f86fbe618e7bbbe5af42d7d6d4ac57a214364db9
Sam Lu <pbcmarsh@gmail.com>
2023-06-21 14:57:40 -0500

    Customer Details PostgreSQL solution
    Finally started this thing, now to make further progress

---
+++ Customer Details.sql
@@ -0,0 +1,14 @@
+SELECT 
+customers.first_name,
+customers.last_name, 
+customers.city,
+orders.order_details
+FROM
+customers
+LEFT JOIN
+orders
+ON
+customers.id = orders.cust_id
+ORDER BY 
+customers.first_name ASC,
+orders.order_details ASC;
