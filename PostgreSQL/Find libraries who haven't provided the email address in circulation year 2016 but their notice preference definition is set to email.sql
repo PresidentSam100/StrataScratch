194fa298e532d9d9986bb826880d2229455af2da
Sam Lu <pbcmarsh@gmail.com>
2023-06-21 17:39:58 -0500

    Create Find libraries who haven't provided the email address in circulation year 2016 but their notice preference definition is set to email.sql
    Solution for PostgreSQL problem with super long name

---
+++ PostgreSQL/Find libraries who haven't provided the email address in circulation year 2016 but their notice preference definition is set to email.sql
@@ -0,0 +1,16 @@
+/*
+ID 9924
+Problem Name: Find libraries who haven't provided the email address in circulation year 2016 but their notice preference definition is set to email
+Link: https://platform.stratascratch.com/coding/9924-find-libraries-who-havent-provided-the-email-address-in-2016-but-their-notice-preference-definition-is-set-to-email?code_type=1
+Tool(s) Used: PostgreSQL
+*/
+SELECT DISTINCT
+home_library_code
+FROM
+library_usage
+WHERE
+year_patron_registered <= 2016
+AND
+notice_preference_definition = 'email'
+AND
+provided_email_address = FALSE
