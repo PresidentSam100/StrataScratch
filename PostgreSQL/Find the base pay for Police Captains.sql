42d8fe079eaafd50597665f9852eade61f22beac
Sam Lu <pbcmarsh@gmail.com>
2023-06-21 17:07:42 -0500

    Create Find the base pay for Police Captains.sql

---
+++ PostgreSQL/Find the base pay for Police Captains.sql
@@ -0,0 +1,13 @@
+/*
+ID 9972
+Problem Name: Find the base pay for Police Captains
+Link: https://platform.stratascratch.com/coding/9972-find-the-base-pay-for-police-captains
+Tool(s) Used: PostgreSQL
+*/
+SELECT
+employeename,
+basepay
+FROM
+sf_public_salaries
+WHERE
+jobtitle = 'CAPTAIN III (POLICE DEPARTMENT)'
