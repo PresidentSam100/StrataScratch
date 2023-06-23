33632ea5b6cdb1e92f4b6f8d831cc4f6d83aa077
Sam Lu <pbcmarsh@gmail.com>
2023-06-23 12:02:50 -0500

    Python Folder StrataScratch

---
+++ Python/Salaries Differences.py
@@ -0,0 +1,15 @@
+"""
+ID 10308
+Problem Name: Salaries Differences
+Link: https://platform.stratascratch.com/coding/10308-salaries-differences?code_type=2
+Tool(s) Used: Python
+"""
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+db = db_employee.merge(db_dept, left_on='department_id', right_on='id')
+m = db[db.department == 'marketing']['salary'].max()
+e = db[db.department == 'engineering']['salary'].max()
+abs(m - e)
