bd85c4a2932e6d0c84bbb5f05ceb8c0979099477
Sam Lu <pbcmarsh@gmail.com>
2023-06-22 22:29:20 -0500

    Most Lucrative Products Solution
    Most Lucrative Products Solution

---
+++ PostgreSQL/Most Lucrative Products.sql
@@ -0,0 +1,18 @@
+/*
+ID 2119
+Problem Name: Most Lucrative Products
+Link: https://platform.stratascratch.com/coding/2119-most-lucrative-products?code_type=1
+Tool(s) Used: PostgreSQL
+*/
+SELECT 
+product_id,
+SUM(cost_in_dollars * units_sold) AS revenue
+FROM
+online_orders
+WHERE
+1 <= EXTRACT(MONTH FROM date) AND EXTRACT(MONTH FROM date) <= 6
+GROUP BY
+product_id
+ORDER BY
+revenue DESC
+LIMIT 5
