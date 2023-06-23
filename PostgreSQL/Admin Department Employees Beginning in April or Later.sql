aeae07f2f1aa92217155d246389921b54e6e3d3d
Sam Lu <pbcmarsh@gmail.com>
2023-06-22 20:59:13 -0500

    Create Admin Department Employees Beginning in April or Later.sql
    Admin Department Employees Beginning in April or Later

---
+++ PostgreSQL/Admin Department Employees Beginning in April or Later.sql
@@ -0,0 +1,12 @@
+/*
+ID 9845
+Problem Name: Admin Department Employees Beginning in April or Later
+Link: https://platform.stratascratch.com/coding/9845-find-the-number-of-employees-working-in-the-admin-department?code_type=1
+Tool(s) Used: PostgreSQL
+*/
+SELECT
+COUNT(*)
+FROM
+worker
+WHERE
+EXTRACT(MONTH FROM joining_date) >= 4 AND department = 'Admin'
