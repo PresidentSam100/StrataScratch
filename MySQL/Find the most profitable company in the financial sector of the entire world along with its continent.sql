5fbf0b4ed585a3868e4b5b4f1eff83a243224588
Sam Lu <pbcmarsh@gmail.com>
2023-12-12 15:03:04 -0600

    Update Find the most profitable company in the financial sector of the entire world along with its continent.sql

--- MySQL/Find the most profitable company in the financial sector of the entire world along with its continent.sql
+++ MySQL/Find the most profitable company in the financial sector of the entire world along with its continent.sql
@@ -1,3 +1,9 @@
+/*
+ID 9663
+Problem Name: Find the most profitable company in the financial sector of the entire world along with its continent
+Link: https://platform.stratascratch.com/coding/9663-find-the-most-profitable-company-in-the-financial-sector-of-the-entire-world-along-with-its-continent?code_type=3
+Tool(s) Used: MySQL
+*/
 SELECT
     company,
     continent
