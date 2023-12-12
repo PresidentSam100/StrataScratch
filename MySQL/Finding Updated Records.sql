9353a7403b45ff795459bd9f36e440b38e451d34
Sam Lu <pbcmarsh@gmail.com>
2023-12-12 13:29:24 -0600

    Create Finding Updated Records.sql

---
+++ MySQL/Finding Updated Records.sql
@@ -0,0 +1,14 @@
+SELECT
+    id,
+    first_name,
+    last_name,
+    department_id,
+    max(salary)
+FROM
+    ms_employee_salary
+GROUP BY
+    first_name,
+    last_name,
+    department_id
+ORDER BY
+    id ASC;
