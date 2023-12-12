278ff4698ffea43eab4d9552ae0b9c6fce4067cf
Sam Lu <pbcmarsh@gmail.com>
2023-12-12 14:55:27 -0600

    Update Find the base pay for Police Captains.sql

--- MySQL/Find the base pay for Police Captains.sql
+++ MySQL/Find the base pay for Police Captains.sql
@@ -1,3 +1,9 @@
+/*
+ID 9972
+Problem Name: Find the base pay for Police Captains
+Link: https://platform.stratascratch.com/coding/9972-find-the-base-pay-for-police-captains?code_type=3
+Tool(s) Used: MySQL
+*/
 SELECT
     employeename,
     basepay
