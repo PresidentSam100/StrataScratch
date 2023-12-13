c23fe4e795f1f00507d1e80e9e19371d4d66b832
Sam Lu <pbcmarsh@gmail.com>
2023-12-13 17:35:55 -0600

    Update Admin Department Employees Beginning in April or Later.r

--- R/Admin Department Employees Beginning in April or Later.r
+++ R/Admin Department Employees Beginning in April or Later.r
@@ -1,11 +1,11 @@
 "
 ID 9845
-Problem Name Admin Department Employees Beginning in April or Later
-Link httpsplatform.stratascratch.comcoding9845-find-the-number-of-employees-working-in-the-admin-departmentcode_type=4
-Tool(s) Used R
+Problem Name: Admin Department Employees Beginning in April or Later
+Link: https://platform.stratascratch.com/coding/9845-find-the-number-of-employees-working-in-the-admin-department?code_type=4
+Tool(s) Used: R
 "
 
 library(dplyr)
-worker %%
-    filter(month(joining_date) = 4, department == 'Admin') %%
+worker %>%
+    filter(month(joining_date) >= 4, department == 'Admin') %>%
     count();
