2cd8a6aaba6810be399b04c577cc56d2817bbb50
Sam Lu <pbcmarsh@gmail.com>
2023-12-14 10:07:49 -0600

    Create Activity Rank.sql

---
+++ PostgreSQL/Medium/Activity Rank.sql
@@ -0,0 +1,20 @@
+/*
+ID 10351
+Problem Name: Activity Rank
+Link: https://platform.stratascratch.com/coding/10351-activity-rank?code_type=1
+Tool(s) Used: PostgreSQL
+*/
+SELECT
+from_user,
+COUNT(*) AS total_emails,
+ROW_NUMBER() OVER ( 
+    ORDER BY
+    COUNT(*) DESC,
+    from_user ASC
+)
+FROM
+google_gmail_emails
+GROUP BY
+from_user
+ORDER BY
+3;
