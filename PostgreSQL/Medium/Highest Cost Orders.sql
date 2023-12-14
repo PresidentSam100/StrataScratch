137f64bd801bf1f005d89bf348061527ee238bb1
Sam Lu <pbcmarsh@gmail.com>
2023-12-14 11:54:26 -0600

    Create Highest Cost Orders.sql

---
+++ PostgreSQL/Medium/Highest Cost Orders.sql
@@ -0,0 +1,25 @@
+/*
+ID 9915
+Problem Name: Highest Cost Orders
+Link: https://platform.stratascratch.com/coding/9915-highest-cost-orders?code_type=1
+Tool(s) Used: PostgreSQL
+*/
+SELECT
+    c.first_name,
+    SUM(o.total_order_cost) AS total_order_cost,
+    o.order_date
+FROM
+    orders AS o
+JOIN
+    customers AS c
+ON
+    o.cust_id = c.id
+WHERE
+    o.order_date BETWEEN
+    '2019-02-01' AND '2019-05-01'
+GROUP BY
+    c.first_name,
+    order_date
+ORDER BY    
+    total_order_cost DESC
+LIMIT 1;
