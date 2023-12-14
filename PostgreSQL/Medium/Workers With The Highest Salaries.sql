d1dcb1ed5e587236a61171b0d14b85119b09f894
Sam Lu <pbcmarsh@gmail.com>
2023-12-13 18:22:51 -0600

    Create Workers With The Highest Salaries.sql

---
+++ PostgreSQL/Medium/Workers With The Highest Salaries.sql
@@ -0,0 +1,17 @@
+/*
+ID 10353
+Problem Name: Workers With The Highest Salaries
+Link: https://platform.stratascratch.com/coding/10353-workers-with-the-highest-salaries?code_type=1
+Tool(s) Used: PostgreSQL
+*/
+SELECT
+    t.worker_title AS best_paid_title
+FROM
+    worker AS w
+JOIN
+    title AS t
+ON
+    w.worker_id = t.worker_ref_id
+ORDER BY
+    w.salary DESC
+FETCH FIRST ROWS WITH TIES;
