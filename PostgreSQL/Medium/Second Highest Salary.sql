2e65e2b77fdabb82be6e3ef05f9128061fc25412
Sam Lu <pbcmarsh@gmail.com>
2023-12-14 14:58:44 -0600

    Create Second Highest Salary.sql

---
+++ PostgreSQL/Medium/Second Highest Salary.sql
@@ -0,0 +1,17 @@
+/*
+ID 9892
+Problem Name: Second Highest Salary
+Link: https://platform.stratascratch.com/coding/9892-second-highest-salary?code_type=1
+Tool(s) Used: PostgreSQL
+*/
+SELECT DISTINCT
+    MAX(salary)
+FROM
+    employee
+WHERE
+    salary != (
+        SELECT
+            MAX(salary) 
+        FROM
+            employee
+    );
