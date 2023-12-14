86e4490a05c19ae7527fef0e4e60f299c241ccce
Sam Lu <pbcmarsh@gmail.com>
2023-12-14 11:58:39 -0600

    Create Highest Target Under Manager.sql

---
+++ PostgreSQL/Medium/Highest Target Under Manager.sql
@@ -0,0 +1,16 @@
+/*
+ID 9905
+Problem Name: Highest Target Under Manager
+Link: https://platform.stratascratch.com/coding/9905-highest-target-under-manager?code_type=1
+Tool(s) Used: PostgreSQL
+*/
+SELECT
+    first_name,
+    target
+FROM
+    salesforce_employees
+WHERE
+    manager_id = 13
+ORDER BY
+    target DESC
+FETCH FIRST ROWS WITH TIES;
